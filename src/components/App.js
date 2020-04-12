8b419260e82f19d2b182b71527bcb03c4a0b98b7
prem-io <Premk.lf@gmail.com>
2020-04-12 16:20:01 +0530

    initial-project-setup

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="App">
+      Would You Rather
+    </div>
+  )
+}
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import 'semantic-ui-css/semantic.min.css'
+import './index.css'
+import App from './components/App'
+
+import { createStore, compose } from 'redux'
+import { Provider } from 'react-redux'
+import rootReducer from './reducers'
+import middleware from './middleware'
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const createStoreWithMiddleware = composeEnhancers(middleware)(createStore)
+
+const store = createStoreWithMiddleware(rootReducer)
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+)
