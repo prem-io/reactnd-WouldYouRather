7f0ff27f7c2fe972e6f871f46c1b83808553d628
prem-io <Premk.lf@gmail.com>
2020-04-18 20:41:31 +0530

    feat: leaderboard api data integrated

--- src/components/LeadCard.js
+++ src/components/LeadCard.js
@@ -3,7 +3,7 @@ import { Card } from 'antd'
 import React from 'react'
 import ImageCard from './ImageCard'
 
-const LeadCard = ({ username = 'Joeylene', url = 'https://reactnd-would-you-rather.netlify.com/images/avatars/lion.png' }) => {
+const LeadCard = ({ user: { name, avatarURL, answerCount, questionCount, total }, medal }) => {
   return (
     <Card
       style={{ marginTop: 16 }}
@@ -12,26 +12,26 @@ const LeadCard = ({ username = 'Joeylene', url = 'https://reactnd-would-you-rath
     >
       <div className="row m-0">
         <div className="col-3 line">
-          <ImageCard url={url} large={true} />
+          <ImageCard url={avatarURL} large={true} />
         </div>
         <div className="col-6 line">
-          <h5 className="text-left mb-3">{username}</h5>
+          <h5 className="text-left mb-3">{name}</h5>
           <div className="row">
             <div className="col-9 py-2">Answered Questions</div>
-            <div className="col-3 py-2">4</div>
+            <div className="col-3 py-2">{answerCount}</div>
           </div>
           <div className="row">
             <div className="col-9 py-2">Created Questions</div>
-            <div className="col-3 py-2">3</div>
+            <div className="col-3 py-2">{questionCount}</div>
           </div>
         </div>
         <div className="col-3">
           <Card type="inner" title="Score" className="lead-score-card">
-            <div className="lead-score">6</div>
+            <div className="lead-score">{total}</div>
           </Card>
         </div>
       </div>
-      <div className="icon gold-medal"></div>
+      <div className={`icon ${medal}`}></div>
     </Card>
   )
 }
--- src/components/LeaderBoard.js
+++ src/components/LeaderBoard.js
@@ -1,16 +1,35 @@
 import React, { Component } from 'react'
 import LeadCard from './LeadCard'
+import { connect } from 'react-redux'
 
 export class LeaderBoard extends Component {
   render() {
+    const { leaderBoardUsers } = this.props
+    const medals = ['gold-medal', 'silver-medal', 'copper-medal']
+
     return (
       <div className="row questions-container">
-        <div className="col-12 br">
-          <LeadCard />
+        <div className="col-12">
+          {leaderBoardUsers.map((user, index) => <LeadCard key={user.id} medal={medals[index]} user={user} />)}
         </div>
       </div>
     )
   }
 }
 
-export default LeaderBoard
+function mapStateToProps({ users }) {
+  const leaderBoardUsers = Object.values(users)
+    .map(user => ({
+      id: user.id,
+      name: user.name,
+      avatarURL: user.avatarURL,
+      answerCount: Object.values(user.answers).length,
+      questionCount: user.questions.length,
+      total: Object.values(user.answers).length + user.questions.length
+    }))
+    .sort((a, b) => b.total - a.total)
+
+  return { leaderBoardUsers }
+}
+
+export default connect(mapStateToProps)(LeaderBoard)
